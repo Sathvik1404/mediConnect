f54d24365f77f026873ed7f2c6b88471f4808b47
inamruth <busettynamruth17@gmail.com>
2024-07-28 20:06:52 +0530

    doclogi

---
+++ client/src/components/doctor/login/DLogin.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+const DLogin = () => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <div className="container">
+      <button onClick={toggleTheme} className="toggle-button">
+        {theme === 'light' ? '🌙' : '☀️'}
+      </button>
+      <div className="card">
+        <div className="card-body">
+          <label>Email</label>
+          <input type="text"></input>
+          <br></br>
+          <label>Password</label>
+          <input type="password"></input>
+          <br></br>
+          <input className="btn btn-primary" type="submit" value="Submit"></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DLogin
