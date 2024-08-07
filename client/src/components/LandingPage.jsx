00dd7074d34e681b8876fa0f111d647a76055c97
sathvik1404 <golisathvik04@gmail.com>
2024-08-08 00:12:34 +0530

    landingpage

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './LandingPage.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import img from './user-doctor-solid (1).svg'
+import pat from './hospital-user-solid (1).svg'
 const LandingPage = () => {
     return (
         <div className="landing-page">
+            <div className="doctor-card">
+                <img src={img} alt="doctor" height={"125px"} width={"125px"} />
+                <h1>Doctor</h1>
+            </div>
+            <div className="patient-card">
+                <img src={pat} alt="patient" height={"125px"} width={"125px"} />
+                <h1>Patient</h1>
+
+            </div>
             <div className="card card-left">
                 <h1>medi</h1>
                 <p>Your Health Is</p>
@@ -15,8 +25,6 @@ const LandingPage = () => {
                 <h1>Connect</h1>
                 <p>Our Priority</p>
             </div>
-            {/* <div className="link-line"> */}
-            <FontAwesomeIcon icon="fa-solid fa-link" />
         </div>
     );
 };
