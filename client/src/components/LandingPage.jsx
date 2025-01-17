58e21fc1d17ac00d5c8f27521010c3199303d3fe
Sathvik <golisathvik04@gmail.com>
2025-01-17 22:07:14 +0530

    Landing Page Updated

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import img from './user-doctor-solid (1).svg';
 import pat from './hospital-user-solid (1).svg';
 import hsp from './hospital-solid.svg';
+import { MDBCarousel, MDBCarouselItem, MDBCarouselCaption } from 'mdb-react-ui-kit';
 
 const LandingPage = () => {
     const navigate = useNavigate();
@@ -19,14 +20,55 @@ const LandingPage = () => {
     const toHospital = () => {
         navigate('/hospital/Hlogin');
     };
-
+    const handleChange = (event) => {
+        const selectedValue = event.target.value;
+        if (selectedValue == "Doctor") {
+            toDoc();
+        } else if (selectedValue == "Patient") {
+            toPatient();
+        } else if (selectedValue == "Hospital") {
+            toHospital();
+        }
+    }
     return (
         <div className="landing-page container-fluid">
-            <header className="text-center my-4">
-                <h1 className="display-4  mt-2">mediConnect</h1>
-                <marquee className="text-warning">Where Patients and Doctors Connect Together</marquee>
+            <header className="text-center mb-2" >
+                <img src="https://tse1.mm.bing.net/th?id=OIP.d_YIGvnUUzIk7Sq_0pGGPQHaC-&pid=Api&P=0&h=180" alt="mediConnectImage" srcset="" style={{ height: '100px', width: '200px', objectFit: 'contain' }} />
+                <h1 >mediConnect</h1>
+                <div className='dropdown'>
+                    <select name='Login' onChange={handleChange}>
+                        <option value="Login">👤 Sign in</option>
+                        <option value="Patient">👨🏻‍💼 Patient Login</option>
+                        <option value="Doctor" >👨‍⚕️ Doctor Login</option>
+                        <option value="Hospital">🏥 Hospital Login</option>
+                    </select>
+                </div>
             </header>
+            <MDBCarousel showIndicators showControls fade>
+                <MDBCarouselItem itemId={1}>
+                    <img src='https://images.pexels.com/photos/7579831/pexels-photo-7579831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' className='d-block w-100' alt='...' style={{ height: '470px', width: '400px', objectFit: 'cover' }} />
+                    <MDBCarouselCaption>
+                        <h5 style={{ color: 'black', }}>Friendly Hosptilising</h5>
+                        <p style={{ color: 'black', }}>Doctors Friendly Interaction with Patients</p>
+                    </MDBCarouselCaption>
+                </MDBCarouselItem>
 
+                <MDBCarouselItem itemId={2}>
+                    <img src='https://images.pexels.com/photos/4225920/pexels-photo-4225920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' className='d-block w-100' alt='...' style={{ height: '470px', width: '400px', objectFit: 'cover' }} />
+                    <MDBCarouselCaption>
+                        <h5 style={{ color: 'black', }}>Patient Interaction</h5>
+                        <p style={{ color: 'black', }}>Consulting Doctor via Online Mode</p>
+                    </MDBCarouselCaption>
+                </MDBCarouselItem>
+
+                <MDBCarouselItem itemId={3}>
+                    <img src='https://images.pexels.com/photos/7195117/pexels-photo-7195117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' className='d-block w-100' alt='...' style={{ height: '470px', width: '400px', objectFit: 'cover' }} />
+                    <MDBCarouselCaption>
+                        <h5 style={{ color: 'black', }}>Medicine Recommendation</h5>
+                        <p style={{ color: 'black', }}>Doctors Suggest Medicines to Patients via Online Mode.</p>
+                    </MDBCarouselCaption>
+                </MDBCarouselItem>
+            </MDBCarousel>
             <div className="hero-section text-center mb-5">
                 <h2>Welcome to Your Health Hub</h2>
                 <p>Connecting Patients, Doctors, and Hospitals for Better Care</p>
@@ -34,7 +76,7 @@ const LandingPage = () => {
             </div>
 
             {/* Cards Container */}
-            <div className="row justify-content-center mb-2">
+            {/* <div className="row justify-content-center mb-2">
                 <div className="col-md-4 mb-4">
                     <div className="card text-center hospital-card pt-5" onClick={toPatient}>
                         <img src={pat} alt="Illustration of a patient" className="card-img-top" />
@@ -62,7 +104,7 @@ const LandingPage = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             {/* Testimonials Section */}
             <section className="testimonials text-center mb-4 ">
@@ -75,7 +117,7 @@ const LandingPage = () => {
                 <p>&copy; 2024 mediConnect. All Rights Reserved.</p>
                 {/* Add social media links here */}
             </footer>
-        </div>
+        </div >
     );
 };
 
